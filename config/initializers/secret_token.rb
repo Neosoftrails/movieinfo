62d131c7c39c8c30fd452b15bb207bf74b00889d
webwerks <webwerks@webwerks-G31M-ES2L.(none)>
2012-09-17 19:24:59 +0530

    basic ui using bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movieinfo::Application.config.secret_token = '6515e6a634330049ed9d09950d417de1c70c9e811b8b06125662bf48571f5f7332898e4a0fef2f372d8712ec88f7031add5b9b38b06062243b2efcdbb9818b45'
